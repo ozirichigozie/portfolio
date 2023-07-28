8c3c58dde8d4ad0f8d4d02b0fa51710c31ca0820
Chigozie <blessedchigozie22@gmail.com>
2023-07-28 08:32:41 +0100

    removed escape function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import openpyxl
-from markupsafe import escape
+# from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -35,7 +35,7 @@ def get_userdata():
 
 @app.route('/<name>', methods=["GET", "POST"])
 def homepage(name):
-    print(f"You are welcome, {escape(name)}!")
+    print(f"You are welcome, {name}!")
     if request.method == "POST":
         return get_userdata()
     return render_template("index.html")
